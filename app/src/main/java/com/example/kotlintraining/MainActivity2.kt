1f186cfa643da74dc2dbfd524ad2a3e777bff126
Patrick <patrickdasilvagonzaga@gmail.com>
2025-06-25 12:01:18 -0300

    criei uma nova act

--- app/src/main/java/com/example/kotlintraining/MainActivity2.kt
+++ app/src/main/java/com/example/kotlintraining/MainActivity2.kt
@@ -1,27 +1,23 @@
 package com.example.kotlintraining
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.kotlintraining.databinding.ActivityMainBinding
+import com.example.kotlintraining.databinding.ActivityMain2Binding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityMain2Binding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.btnOpenNewAct.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        binding.fechar.setOnClickListener{
+            finish()
         }
     }
-
-
 }
